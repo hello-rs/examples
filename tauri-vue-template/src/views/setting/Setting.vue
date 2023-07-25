adb60b88c5264f889870e901cfd6bb0bbee5efb8
sunwenming <645187582@qq.com>
2023-07-25 22:08:24 +0800

    1

--- tauri-vue-template/src/views/setting/Setting.vue
+++ tauri-vue-template/src/views/setting/Setting.vue
@@ -1,18 +1,11 @@
 <script setup lang="ts">
 import { t } from "@/locales";
 import { Language, Theme, useAppStore } from "@/store";
-import { MenuOption, NButton, NSelect, NLayout, NLayoutSider, NLayoutContent, NMenu, useMessage } from "naive-ui";
+import { NButton, NSelect, NCard, NTabs, NTabPane, NSpace, useMessage } from "naive-ui";
 
 const appStore = useAppStore()
 const ms = useMessage()
 
-// 菜单
-const menuOptions: MenuOption[] = [
-  {
-    label: "App设置",
-    key: "app",
-  },
-];
 // 主题选项
 const themeOptions: { label: string; key: Theme }[] = [
   {
@@ -40,45 +33,55 @@ const languageOptions: { label: string; key: Language; value: Language }[] = [
   { label: "Italiano", key: "it-IT", value: "it-IT" },
 ];
 // menu 点击事件
-function cliMenu(key: string, item: MenuOption): void {
-  ms.info("[onUpdate:value]: " + JSON.stringify(key));
+function cliMenu(item: { label: string; key: string }): void {
   ms.info("[onUpdate:value]: " + JSON.stringify(item));
 }
 // 多语言点击事件
 function cli_reset() {
+  console.log("??")
   ms.success(t('common.success'))
   window.location.reload()
 }
 </script>
 
 <template>
-  <div class="flex flex-row">
-    <NMenu :options="menuOptions" @update:value="cliMenu" />
-    <template>
-      <div class="flex items-center space-x-4">
-        <span class="flex-shrink-0 w-[100px]">{{ $t('setting.theme') }}</span>
-        <div class="flex flex-wrap items-center gap-4">
-          <template v-for="item of themeOptions" :key="item.key">
-            <NButton size="small" :type="item.key === appStore.app.theme ? 'primary' : undefined"
-              @click="appStore.set_theme(item.key)">
-              {{ item.label }}
-            </NButton>
-          </template>
-        </div>
-      </div>
-      <div class="flex items-center space-x-4">
-        <span class="flex-shrink-0 w-[100px]">{{ $t('setting.language') }}</span>
-        <div class="flex flex-wrap items-center gap-4">
-          <NSelect :value="appStore.app.language" :options="languageOptions"
-            @update-value="(value: string) => appStore.set_language(value as Language)" />
-        </div>
-      </div>
-      <div class="flex items-center space-x-4">
-        <span class="flex-shrink-0 w-[100px]">{{ $t('setting.resetUserInfo') }}</span>
-        <NButton size="small" @click="cli_reset">
-          {{ $t('common.reset') }}
-        </NButton>
-      </div>
-    </template>
+  <div>
+    <n-card title="设置">
+      <n-tabs default-value="app" size="large" animated type="line">
+        <n-tab-pane name="app" tab="App 设置">
+          <n-space vertical>
+            <n-space>
+              <span class="flex-shrink-0 w-[100px]">{{ $t('setting.theme') }}</span>
+              <div class="flex flex-wrap items-center gap-4">
+                <template v-for="item of themeOptions" :key="item.key">
+                  <NButton size="small" :type="item.key === appStore.app.theme ? 'primary' : undefined"
+                    @click="appStore.set_theme(item.key)">
+                    {{ item.label }}
+                  </NButton>
+                </template>
+              </div>
+            </n-space>
+            <n-space>
+              <span class="flex-shrink-0 w-[100px]">{{ $t('setting.language') }}</span>
+              <div class="flex flex-wrap items-center gap-4">
+                <NSelect :value="appStore.app.language" :options="languageOptions"
+                  @update-value="(value: string) => appStore.set_language(value as Language)" />
+              </div>
+            </n-space>
+            <n-space>
+              <span class="flex-shrink-0 w-[100px]">{{ $t('setting.resetUserInfo') }}</span>
+              <NButton size="small" @click="cli_reset">
+                {{ $t('common.reset') }}
+              </NButton>
+            </n-space>
+          </n-space>
+        </n-tab-pane>
+        <n-tab-pane name="signup" tab="注册">
+          <n-button type="primary" block secondary strong>
+            注册
+          </n-button>
+        </n-tab-pane>
+      </n-tabs>
+    </n-card>
   </div>
 </template>
