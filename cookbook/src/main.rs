e27cb8f32b9c4d7ecafa97e1862df1d975b04aa0
sunwenming <645187582@qq.com>
2023-08-27 17:03:36 +0800

    del

--- cookbook/src/main.rs
+++ cookbook/src/main.rs
@@ -1,18 +1,3 @@
-use std::time::{Duration, Instant};
-
 mod codes;
-use chrono::{prelude::*, LocalResult, ParseResult};
-use codes::ctime;
 fn main() {
-    let start = ctime::start_elapsed();
-    println!("{}", ctime::now());
-    println!("{}", ctime::timestamp());
-    println!("{}", ctime::string());
-    println!("{}", ctime::timestamp_to_time(ctime::timestamp()).unwrap());
-    println!(
-        "{}",
-        ctime::string_to_time("2023-01-01 11:11:11")
-            .unwrap()
-    );
-    println!("{:?}", ctime::end_elapsed(start));
 }
